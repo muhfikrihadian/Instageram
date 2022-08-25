c04fdffd5a0c8844822cf4dff552ff3e6d5c1742
muhfikrihadian <muhfikrihadian@gmail.com>
2022-08-25 15:30:38 +0700

    Initial Commit

---
+++ Instageram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instageram
+//
+//  Created by Muhammad Fikri Hadian on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instageram/InstageramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstageramApp.swift
+//  Instageram
+//
+//  Created by Muhammad Fikri Hadian on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct InstageramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
